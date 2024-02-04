3e555f9f11172d4bc2b3b6d565b02b346bef6e00
FelipeGonzalezD29 <141456458+FelipeGD29@users.noreply.github.com>
2024-02-04 18:24:55 -0300

    Footer y Abilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero";
 // import SideNavBar from "./components/SideNavBar";
+import MyAbilities from "./components/MyAbilities";
+import Footer from "./components/Footer";
 const App = (): JSX.Element => {
   return (
     <main className="font-Comfortaa font-normal">
       {/* <SideNavBar/> */}
       <Hero/>
+      <MyAbilities/>
+      <Footer/>
     </main>
   );
 };
