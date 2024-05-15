561c070225cd1f95068b3a3c0629d4f248960a87
FelipeGonzalezD29 <141456458+FelipeGD29@users.noreply.github.com>
2024-05-15 00:46:19 -0300

    Climb T project in myProjects update

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -1,5 +1,6 @@
 import myWorld from "../assets/myWorld.png";
 import myVideo from "../assets/myVideo.webm";
+import ClimbTVideo from "../assets/ClimbTVideo.mp4";
 
 const MyProyects = () => {
   return (
@@ -7,7 +8,38 @@ const MyProyects = () => {
       id="myProjects-section"
       className="2xl:m-20 xl:m-20 lg:m-10 md:m-5 sm:my-60 sm:mb-5 flex flex-col 2xl:gap-36 xl:gap-30 lg:gap-20 md:gap-5"
     >
-      <h2 className="myHeader text-3xl 2xl:-mb-14 xl:-mb-16 lg:-mb-16 md:mb-5 sm:mb-5">Mis proyectos</h2>
+      <h2 className="myHeader text-3xl 2xl:-mb-14 xl:-mb-16 lg:-mb-16 md:mb-5 sm:mb-5">
+        Mis proyectos
+      </h2>
+      <article className="bg-gradient-to-b from-red to-purple flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-col-reverse sm:flex-col-reverse gap-5 rounded-2xl border-4 p-5">
+        <div>
+          <h2 className="text-2xl underline text-black hover:text-purple transition-colors duration-500">
+            Climb T website
+          </h2>
+          <p>
+            Climb T necesitaba actualizar su sitio web con un diseño más moderno
+            y motivador, capaz de inspirar a sus clientes a alcanzar el éxito en
+            sus negocios. Esto incluía asegurarse de que el diseño fuera
+            receptivo y atractivo.
+          </p>
+          <p>
+            Me encargué de diseñar la estructura del sitio, seleccionando
+            cuidadosamente las imágenes y mensajes para transmitir de manera
+            precisa, visual y efectiva la visión de la empresa.
+          </p>
+          <p>
+            Además, desarrollé diseños responsivos adaptados a diversos tamaños
+            de pantalla, garantizando una experiencia óptima para los usuarios
+            en cualquier dispositivo.
+          </p>
+        </div>
+        <video
+          src={ClimbTVideo}
+          controls
+          className="2xl:w-1/2 xl:w-1/2 lg:w-1/2 rounded-2xl"
+        />
+      </article>
+
       <article className="bg-gradient-to-b from-red to-purple flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col gap-5 rounded-2xl border-4 p-5">
         <a
           href="https://myworld-72wn.onrender.com"
